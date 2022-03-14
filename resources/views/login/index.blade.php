f54263d7314fac5ce6d2dabd990a5a9dcf1f3f94
Ibrahim <rahadian.ibrahim@gmail.com>
2022-03-14 20:15:39 +0700

    removed forgot password

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -15,6 +15,17 @@
               </div>
           </div>
           @endif
+          @if (session('error'))
+          <div class="card-body">
+            <div class="alert alert-danger alert-dismissible show fade">
+              <div class="alert-body">
+                <button class="close" data-dismiss="alert">
+                  <span>&times;</span>
+                </button>
+                {{ session('error') }}
+              </div>
+          </div>
+          @endif
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
 
             <div class="login-brand">
@@ -38,11 +49,6 @@
                   <div class="form-group">
                     <div class="d-block">
                     	<label for="password" class="control-label">Password</label>
-                      <div class="float-right">
-                        <a href="auth-forgot-password.html" class="text-small">
-                          Forgot Password?
-                        </a>
-                      </div>
                     </div>
                     <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
                     <div class="invalid-feedback">
@@ -66,7 +72,7 @@
               </div>
             </div>
             <div class="mt-5 text-muted text-center">
-              Tidak punya akun? <a href="auth-register.html">Daftar disini</a>
+              Tidak punya akun? <a href="{{route('registrasi')}}">Daftar disini</a>
             </div>
             <div class="simple-footer">
               Copyright &copy; Stisla 2018
